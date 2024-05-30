6defcee871b87a40a16d909c2fab010ae59d1add
IREG-IT <support@marys-team.com>
2024-05-30 21:19:56 +0500

    Saas Pegasus Code

---
+++ React/vast_projet/api-client/models/index.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AggregateEmployeeData';
+export * from './CustomUser';
+export * from './DepartmentEnum';
+export * from './Employee';
+export * from './JWT';
+export * from './Login';
+export * from './PaginatedEmployeeList';
+export * from './PatchedCustomUser';
+export * from './PatchedEmployee';
+export * from './Register';
+export * from './RestAuthDetail';
+export * from './TokenRefresh';
+export * from './TokenVerify';
+export * from './UserSignupStats';
---
+++ React/vast_projet/vast_projet/settings.py
@@ -0,0 +1,390 @@
+"""
+Django settings for My VAST App project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/stable/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/stable/ref/settings/
+"""
+
+import os
+from datetime import timedelta
+from pathlib import Path
+
+import environ
+from django.utils.translation import gettext_lazy
+
+# Build paths inside the project like this: BASE_DIR / "subdir".
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, ".env"))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/stable/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("SECRET_KEY", default="oHCZeHhzBGstdEGGDHJJDLVrPJoRrUWghrCyZdEA")
+
+# SECURITY WARNING: don"t run with debug turned on in production!
+DEBUG = env.bool("DEBUG", default=True)
+
+# Note: It is not recommended to set ALLOWED_HOSTS to "*" in production
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
+
+
+# Application definition
+
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sitemaps",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "django.forms",
+]
+
+# Put your third-party apps here
+THIRD_PARTY_APPS = [
+    "allauth",  # allauth account/registration management
+    "allauth.account",
+    "allauth.socialaccount",
+    "django_otp",
+    "django_otp.plugins.otp_totp",
+    "django_otp.plugins.otp_static",
+    "allauth.mfa",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "rest_framework_simplejwt",
+    "corsheaders",
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
+    "drf_spectacular",
+    "celery_progress",
+    "waffle",
+]
+
+PEGASUS_APPS = [
+    "pegasus.apps.examples.apps.PegasusExamplesConfig",
+    "pegasus.apps.employees.apps.PegasusEmployeesConfig",
+]
+
+# Put your project-specific apps here
+PROJECT_APPS = [
+    "apps.authentication.apps.AuthenticationConfig",
+    "apps.users.apps.UserConfig",
+    "apps.dashboard.apps.DashboardConfig",
+    "apps.web",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PEGASUS_APPS + PROJECT_APPS
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
+    "apps.web.locale_middleware.UserLocaleMiddleware",
+    "apps.web.locale_middleware.UserTimezoneMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "waffle.middleware.WaffleMiddleware",
+]
+
+
+ROOT_URLCONF = "vast_projet.urls"
+
+
+# used to disable the cache in dev, but turn it on in production.
+# more here: https://nickjanetakis.com/blog/django-4-1-html-templates-are-cached-by-default-with-debug-true
+_DEFAULT_LOADERS = [
+    "django.template.loaders.filesystem.Loader",
+    "django.template.loaders.app_directories.Loader",
+]
+
+_CACHED_LOADERS = [("django.template.loaders.cached.Loader", _DEFAULT_LOADERS)]
+
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "apps.web.context_processors.project_meta",
+                # this line can be removed if not using google analytics
+                "apps.web.context_processors.google_analytics_id",
+            ],
+            "loaders": _DEFAULT_LOADERS if DEBUG else _CACHED_LOADERS,
+        },
+    },
+]
+
+WSGI_APPLICATION = "vast_projet.wsgi.application"
+
+FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
+
+# Database
+# https://docs.djangoproject.com/en/stable/ref/settings/#databases
+
+if "DATABASE_URL" in env:
+    DATABASES = {"default": env.db()}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": env("DJANGO_DATABASE_NAME", default="vast_projet"),
+            "USER": env("DJANGO_DATABASE_USER", default="postgres"),
+            "PASSWORD": env("DJANGO_DATABASE_PASSWORD", default="***"),
+            "HOST": env("DJANGO_DATABASE_HOST", default="localhost"),
+            "PORT": env("DJANGO_DATABASE_PORT", default="5432"),
+        }
+    }
+
+# Auth / login stuff
+
+# Django recommends overriding the user model even if you don"t think you need to because it makes
+# future changes much easier.
+AUTH_USER_MODEL = "users.CustomUser"
+LOGIN_URL = "account_login"
+LOGIN_REDIRECT_URL = "/"
+
+# Password validation
+# https://docs.djangoproject.com/en/stable/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+# Allauth setup
+
+ACCOUNT_ADAPTER = "apps.users.adapter.EmailAsUsernameAdapter"
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
+ACCOUNT_CONFIRM_EMAIL_ON_GET = True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
+ACCOUNT_SESSION_REMEMBER = True
+ACCOUNT_LOGOUT_ON_GET = True
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
+ACCOUNT_LOGIN_BY_CODE_ENABLED = True
+
+ACCOUNT_FORMS = {
+    "signup": "apps.users.forms.TermsSignupForm",
+}
+
+# User signup configuration: change to "mandatory" to require users to confirm email before signing in.
+# or "optional" to send confirmation emails but not require them
+ACCOUNT_EMAIL_VERIFICATION = env("ACCOUNT_EMAIL_VERIFICATION", default="none")
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    "django.contrib.auth.backends.ModelBackend",
+    # `allauth` specific authentication methods, such as login by e-mail
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+# Internationalization
+# https://docs.djangoproject.com/en/stable/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+LANGUAGE_COOKIE_NAME = "vast_projet_language"
+LANGUAGES = [
+    ("en", gettext_lazy("English")),
+    ("fr", gettext_lazy("French")),
+]
+LOCALE_PATHS = (BASE_DIR / "locale",)
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/stable/howto/static-files/
+
+STATIC_ROOT = BASE_DIR / "static_root"
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        # swap these to use manifest storage to bust cache when files change
+        # note: this may break image references in sass/css files which is why it is not enabled by default
+        # "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/stable/ref/settings/#default-auto-field
+
+# future versions of Django will use BigAutoField as the default, but it can result in unwanted library
+# migration files being generated, so we stick with AutoField for now.
+# change this to BigAutoField if you"re sure you want to use it and aren"t worried about migrations.
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+# Email setup
+
+# use in development
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# use in production
+# see https://github.com/anymail/django-anymail for more details/examples
+# EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+
+EMAIL_SUBJECT_PREFIX = "[My VAST App] "
+
+# Django sites
+
+SITE_ID = 1
+
+# DRF config
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": True,
+    "SIGNING_KEY": env("SIMPLE_JWT_SIGNING_KEY", default="<a comlex signing key>"),
+    "ALGORITHM": "HS512",
+}
+
+REST_AUTH = {
+    "USE_JWT": True,
+    "JWT_AUTH_HTTPONLY": False,
+    "USER_DETAILS_SERIALIZER": "apps.users.serializers.CustomUserSerializer",
+}
+
+CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=["http://localhost:5173"])
+
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "My VAST App",
+    "DESCRIPTION": "Video, Audio, Text and Speech Converter",
+    "VERSION": "0.1.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_SETTINGS": {
+        "displayOperationId": True,
+    },
+}
+
+# Celery setup (using redis)
+if "REDIS_URL" in env:
+    REDIS_URL = env("REDIS_URL")
+elif "REDIS_TLS_URL" in env:
+    REDIS_URL = env("REDIS_TLS_URL")
+else:
+    REDIS_HOST = env("REDIS_HOST", default="localhost")
+    REDIS_PORT = env("REDIS_PORT", default="6379")
+    REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
+
+if REDIS_URL.startswith("rediss"):
+    REDIS_URL = f"{REDIS_URL}?ssl_cert_reqs=none"
+
+CELERY_BROKER_URL = CELERY_RESULT_BACKEND = REDIS_URL
+CELERY_BEAT_SCHEDULE = {
+    "test-celerybeat": {
+        "task": "pegasus.apps.examples.tasks.example_log_task",
+        "schedule": timedelta(minutes=1),
+        "options": {
+            "expires": 60,  # cancel this task after a minute if it hasn't started
+        },
+    },
+}
+
+
+# Pegasus config
+
+# replace any values below with specifics for your project
+PROJECT_METADATA = {
+    "NAME": gettext_lazy("My VAST App"),
+    "URL": "http://localhost:8000",
+    "DESCRIPTION": gettext_lazy("Video, Audio, Text and Speech Converter"),
+    "IMAGE": "https://upload.wikimedia.org/wikipedia/commons/2/20/PEO-pegasus_black.svg",
+    "KEYWORDS": "SaaS, django",
+    "CONTACT_EMAIL": "support@marys-team.com",
+}
+
+# set this to True in production to have URLs generated with https instead of http
+USE_HTTPS_IN_ABSOLUTE_URLS = env.bool("USE_HTTPS_IN_ABSOLUTE_URLS", default=False)
+
+ADMINS = [("Shahzaib", "support@marys-team.com")]
+
+# Add your google analytics ID to the environment to connect to Google Analytics
+GOOGLE_ANALYTICS_ID = env("GOOGLE_ANALYTICS_ID", default="")
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": '[{asctime}] {levelname} "{name}" {message}',
+            "style": "{",
+            "datefmt": "%d/%b/%Y %H:%M:%S",  # match Django server time format
+        },
+    },
+    "handlers": {
+        "console": {"class": "logging.StreamHandler", "formatter": "verbose"},
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": env("DJANGO_LOG_LEVEL", default="INFO"),
+        },
+        "vast_projet": {
+            "handlers": ["console"],
+            "level": env("VAST_PROJET_LOG_LEVEL", default="INFO"),
+        },
+    },
+}
